a2f6f56119f544aa676b1f05c65719644c9611be
GMTurbo <gabe.testa@gmail.com>
2014-05-15 22:32:27 -0600

    added moons

--- js/stars.js
+++ js/stars.js
@@ -43,7 +43,7 @@ function bigBang() {
   con = canvas.getContext('2d');
   stars = WIDTH * 0.25;
   for(var i = 0; i < stars; i++) {
-    pxs[i] = new Star();
+    pxs[i] = new Body();
     pxs[i].reset();
   }
   
@@ -61,17 +61,35 @@ function draw() {
   window.requestAnimationFrame(draw);
 }
 
-colors = ['red', 'blue', 'green', ]
+colors = [
+  'rgb(249,72,78)',
+  'rgb(244,200,161)',
+  'rgb(138,247,189)',
+  'rgb(114,249,119)',
+  
+  'rgb(184,214,212)',
+  'rgb(216,223,211)',
+  'rgb(243,227,210)',
+  'rgb(249,191,166)',
+  'rgb(251,163,163)'
+  
+  ];
 
-function Star() {
-  var planet, x, y, r, dx, dy, dr, life, superfar, dying, fadeIn, opacity;
+function Body() {
+  var planet, x, y, r, dx, dy, dr, life, superfar, dying, fadeIn, opacity, color, moons = [];
   
   this.reset = function(kill) {
   	planet = (Math.random() <= 0.05);
     x = (WIDTH*Math.random());
     y = (HEIGHT*Math.random());
     r = Math.random() * 3 + .1;
-
+    
+    if(planet){
+      r = Math.random() * 5 + 1.0;
+      color = colors[~~(Math.random() * 20) % colors.length];
+      this.createMoons();
+    }
+    
     var shooting_star = (Math.random() <= 0.04);
 
   	if (shooting_star==true) {
@@ -93,8 +111,17 @@ function Star() {
    	opacity = Math.random();
    	
    	superfar = (Math.random() < 0.1);
-  }
-
+  };
+  
+  this.createMoons = function(){
+    var num = ~~(Math.random() * 4);
+    var dis = 8;
+    for(var i = 0 ; i < num ; i++){
+      moons.push(new Moon( dis, ~~(Math.random() * r/2.0) + (r/3.0)));
+      dis += 5;
+    }
+  };
+  
   this.fadeLong = function() {
     if(superfar) return;
     if (fadeIn) {
@@ -117,13 +144,13 @@ function Star() {
       }
     }
   
-  }
+  };
   
   this.draw = function() {
     con.beginPath();
      
     if (planet===true) {
-      con.fillStyle = 'rgba(138,60,57,'+opacity+')';
+      con.fillStyle = color;
       con.shadowColor   = 'rgba(166,40,34,1)';
       con.arc(x - 5, y - 5, 5, 0, 2 * Math.PI, false);
     }
@@ -139,19 +166,57 @@ function Star() {
     con.shadowOffsetY = 0;
     con.shadowBlur    = 10;
     con.fill();
-  }
+    
+    if(moons){
+        moons.forEach(function(moon){
+          moon.draw();
+        });
+      }
+  };
   
   this.move = function() {
     if(superfar) return;
     x += dx;
     y += dy;
     r += dr;
+    if(moons){
+      moons.forEach(function(moon){
+          moon.move(this);
+        }, this);
+    }
     if(r < 0 || r > 30)
       opacity-=0.025;
     if(x > WIDTH || x < 0 || opacity <= 0 || r < 0) this.reset(kill=true);
     if(y > HEIGHT || y < 0) this.reset(kill=true);
-  }
+  };
 
-  this.getX = function() { return x; }
-  this.getY = function() { return y; }
+  this.getX = function() { return x - 2*r; }
+  this.getY = function() { return y - 2*r; }
 }
+
+var Moon = function(r, size){
+
+  var angle = 0, da = Math.sqrt((2*Math.PI)/(300*r)), x, y, moonR;
+  this.setup = function(){
+      moonR = size/2.0;
+  }();
+  
+  this.move = function(mom){
+    x = mom.getX() + Math.cos(angle) * r;
+    y = mom.getY() + Math.sin(angle) * r;
+    angle += da;
+  };
+  
+  this.draw = function(){
+    con.beginPath();
+    con.fillStyle = 'white';
+    con.shadowColor   = 'rgba(166,40,34,1)';
+    con.arc(x - moonR, y - moonR, moonR, 0, 2 * Math.PI, false);
+    
+    con.closePath();
+    con.shadowOffsetX = 0;
+    con.shadowOffsetY = 0;
+    con.shadowBlur    = 10;
+    con.fill();
+  };
+};
